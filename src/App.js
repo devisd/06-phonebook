b2f5e24535357f3271f56c02c38208f2cfad0f83
devisd <89788799+devisd@users.noreply.github.com>
2022-09-04 21:51:20 +0700

    add start files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Form from './components/Form';
+import Contacts from 'components/Contacts';
+import Filter from 'components/Filter';
+import { nanoid } from 'nanoid';
 
-function App() {
+// ====== Второй способ работы с localStorage и useState ======
+//  == Отдельная функция работы с localStorage и useEffect ==
+// const useLocalStorage = (key, defaultValue) => {
+//   const [state, setState] = useState(() => {
+//     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+//   });
+//   useEffect(() => {
+//     window.localStorage.setItem(key, JSON.stringify(state));
+//   }, [key, state]);
+
+//   return [state, setState];
+// };
+//  == Записи useState в функции ==
+// const [contacts, setContacts] = useState('contacts', []);
+// const [filter, setFilter] = useState('filter', '');
+//
+// =============================================================
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = data => {
+    const newName = contacts.map(el => el.name.toLowerCase());
+    if (newName.includes(data.name.toLowerCase())) {
+      return alert(`${data.name} is already in contacts`);
+    }
+    const input = { id: nanoid(3), name: data.name, number: data.number };
+    setContacts(prevState => [...prevState, input]);
+  };
+
+  const onFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const onCheck = () => {
+    if (filter) {
+      return contacts.filter(el =>
+        el.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    } else {
+      return contacts;
+    }
+  };
+
+  const onDeleteContact = id => {
+    const contactName = contacts.filter(el => el.id !== id);
+    setContacts(contactName);
+  };
+
+  const filteredContacts = onCheck();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+        padding: 50,
+      }}
+    >
+      <h1>Phonebook</h1>
+      <Form onSubmit={formSubmitHandler} />
+      <Filter filter={filter} onChange={onFilter} />
+      <h2>Contacts</h2>
+      <Contacts contacts={filteredContacts} deleteContact={onDeleteContact} />
     </div>
   );
-}
+};
 
 export default App;
